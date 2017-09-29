c2a1c216e27f395ba10ef828f59864d2a13268c9
alan rodrigues <alan@mobilemind.com.br>
2017-09-28 22:24:53 -0300

    SliceIndex

--- support/utils.go
+++ support/utils.go
@@ -30,6 +30,22 @@ func MakeRange(min, max int) []int {
     return a
 }
 
+func SliceIndex(limit int, predicate func(i int) bool) int {
+    for i := 0; i < limit; i++ {
+        if predicate(i) {
+            return i
+        }
+    }
+    return -1
+}
+
+// troca , por .(ponto), posi alterei o js maskMoney pra #.###,##
+func NormalizeSemicolon(key string, ctx *context.Context) {
+	if _, ok := ctx.Request.Form[key]; ok {
+		ctx.Request.Form[key][0] = strings.Replace(ctx.Request.Form[key][0], ",", "", -1)
+	}
+}
+
 func RemoveAllSemicolon(key string, ctx *context.Context) {
 	if _, ok := ctx.Request.Form[key]; ok {
 		ctx.Request.Form[key][0] = strings.Replace(ctx.Request.Form[key][0], ",", "", -1)
