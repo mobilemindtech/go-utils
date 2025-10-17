48664f57f9a168f0248e34610018fd5948b5d307
Ricardo <ricardo@mobilemind.com.br>
2025-10-17 10:30:27 -0300

    fix email validation

--- validator/email/validate.go
+++ validator/email/validate.go
@@ -5,6 +5,7 @@ import (
 )
 
 func IsValid(email string) bool {
-	re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[A-Za-z]{2,}$`)
+	//re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[A-Za-z]{2,4}$`)
 	return re.MatchString(email)
 }
